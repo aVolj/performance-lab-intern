1c92cd4c90365b7a75744ae4efc19324a9696763
aVolj <79371045+aVolj@users.noreply.github.com>
2021-02-22 23:49:28 +0300

    add task 6.1, 6.2

--- src/Cards.java
+++ src/Cards.java
@@ -1,28 +1,30 @@
 public class Cards {
 
-    public static void dealCards(int n, int players){
+    public static void dealCards(int n, int players) {
         String[] rantCards = {"6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
-        String[] suit = {"\u2664", "\u2661", "\u2667", "\u2662" };
+        String[] suit = {"\u2664", "\u2661", "\u2667", "\u2662"};
 
         int cardsInDeck = 36;
         int currentPosition = 0;
         String[] deck = new String[cardsInDeck];
         //TODO добавить проверку кол-ва пользователей
-        for(int i = 0; i < rantCards.length; i++){
-            for(int j = 0; j < suit.length; j++){
+
+        for (int i = 0; i < rantCards.length; i++) {
+            for (int j = 0; j < suit.length; j++) {
                 deck[currentPosition] = rantCards[i] + suit[j];
                 currentPosition++;
             }
         }
-        for(int i = 0; i < deck.length; i++ ){
-            int randomCard = (int)(Math.random()*cardsInDeck-i) + i;
+        for (int i = 0; i < deck.length; i++) {
+            int randomCard = (int) (Math.random() * cardsInDeck - i) + i;
+
             String temp = deck[randomCard];
             deck[randomCard] = deck[i];
             deck[i] = temp;
         }
-        for(int i = 0; i < players; i++ ){
-            for(int j = 0; j < n; j++){
-                System.out.print(deck[j+n*i] + " ");
+        for (int i = 0; i < players; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(deck[j + n * i] + " ");
             }
             System.out.println("\n");
         }
