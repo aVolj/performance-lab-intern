02986776cf189f06d50ffb87a2728264425983da
aVolj <79371045+aVolj@users.noreply.github.com>
2021-02-22 23:42:43 +0300

    add task 6.1, 6.2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 class Main {
     public static void main(String[] args) {
-        CalendarForYear.calendar();
+       // CalendarForYear.calendar();
        // task1_5.printSortEl(10);
+        Cards.dealCards(6, 2);
     }
 }
